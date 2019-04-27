109f09d3bd7cdcfa369d7f3688585c932d54271e
reyco <1548171008@qq.com>
2019-04-27 23:16:48 +0800

    Shiro安全框架

--- Shiro/08shiro/src/com/reyco/core/dao/AccountDao.java
+++
@@ -1,9 +0,0 @@
-package com.reyco.core.dao;
-
-import com.reyco.core.pojo.Account;
-
-public interface AccountDao {
-	
-	public Account searchUserByUserName(String username);
-	
-}
--- Shiro/08shiro/src/com/reyco/core/service/AccountService.java
+++
@@ -1,9 +0,0 @@
-package com.reyco.core.service;
-
-import com.reyco.core.pojo.Account;
-
-public interface AccountService {
-	
-	public Account searchUserByUserName(String username);
-	
-}
